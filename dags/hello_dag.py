fa8eb3eff06f34efb01c6a5e69eec15ab7890d13
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2022-05-12 17:52:09 -0400

    update

--- dags/hello_dag.py
+++ dags/hello_dag.py
@@ -5,7 +5,7 @@ from airflow.operators.python_operator import PythonOperator
 def hello():
     return 'Hello world!'
 
-hello_dag = DAG('hello_world', start_date=datetime.now())
+hello_dag = DAG('hello_world', start_date=datetime(2021, 1, 1))
 
 hello_task = PythonOperator(task_id='hello_task', python_callable=hello, dag=hello_dag)
 
